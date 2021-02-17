a2378abe84af7c0f4e2c437297d4d3172f3ec115
Rogers Ntumwa <ntumwar@gmail.com>
2021-02-17 23:49:17 +0400

    implemented alert sustem

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react"
+import {BrowserRouter as Router, Route} from "react-router-dom"
 import './App.scss';
+import Alert from "./components/Alert";
+import Login from "./screens/Login";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Starting with node sass</h1>
-    </div>
+    <Router>
+      {/* <Alert/> */}
+      <Route path="/" component={Login} exact/>
+    </Router>
+    
   );
 }
 
