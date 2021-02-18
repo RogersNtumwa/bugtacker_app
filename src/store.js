db95192aee76192877d6a0095312006d1d992829
Rogers Ntumwa <ntumwar@gmail.com>
2021-02-18 23:19:09 +0400

    implemented authentication actions and reducers

--- src/store.js
+++ src/store.js
@@ -1,19 +1,20 @@
-import {combineReducers, createStore, applyMiddleware} from "redux"
-import {composeWithDevTools} from "redux-devtools-extension"
+import { combineReducers, createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
+import user from "./reducers/auth";
 
-const middleware= [thunk]
+const middleware = [thunk];
 
-const initialState={}
+const initialState = {};
 
 const reducer = combineReducers({
-    testing:"testtingreducer"
-})
+  auth: user,
+});
 
-const store =createStore(
-    reducer,
-    initialState, 
-    composeWithDevTools(applyMiddleware(...middleware))
-)
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
-export default store
+export default store;
