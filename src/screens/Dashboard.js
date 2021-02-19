17465101acbe39c7b47e944a452245329231a2f7
Rogers Ntumwa <ntumwar@gmail.com>
2021-02-19 14:00:08 +0400

    implemented authourization rights on the dashboard screen

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
 
-const Dashboard = () => {
+const Dashboard = ({ history }) => {
+  const auth = useSelector((state) => state.auth);
+  const { isAuthenticated } = auth;
+  useEffect(() => {
+    if (!isAuthenticated) {
+      return history.push("/");
+    }
+  }, [isAuthenticated, history]);
   return (
     <div>
       <h1>Welicome to My Dashboard</h1>
