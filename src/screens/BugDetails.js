f26c84163a48bdd28a3fd77ebfe3aa36732f7593
Rogers Ntumwa <ntumwar@gmail.com>
2021-03-03 15:05:11 +0400

    added accordian in the bug details page

--- src/screens/BugDetails.js
+++ src/screens/BugDetails.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useEffect, useState } from "react";
-import { Accordion, Button, Card, Container } from "react-bootstrap";
+import { Accordion, Button, Card, Container, Jumbotron } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
@@ -20,13 +20,54 @@ const BugDetails = ({ match }) => {
         <Link className="btn btn-light my-3" to="/dashboard/bugs">
           Go Back
         </Link>
-        {!loading && <h3>Details for Bug:({bug.data._id}) </h3>}
-
-        <Accordion>
-          <Card>
-            <Card.Header></Card.Header>
-          </Card>
-        </Accordion>
+        {loading ? (
+          "loading"
+        ) : (
+          <div>
+            <h2>Details for Bug:({bug.data._id}) </h2>
+            <Jumbotron>
+              <h4 className="title">Title: {bug.data.title}</h4>
+              <p>
+                <h6>Description:</h6> {bug.data.description}.
+              </p>
+              <p>
+                <h6>Status:</h6>
+                {bug.data.status}
+              </p>
+              <p>
+                <h6>Priority:</h6> {bug.data.priority}
+              </p>
+              <p>
+                <h6>Project:</h6> {bug.data.project}
+              </p>
+              <p>
+                <h6>AssignedTo:</h6> {bug.data.assignedTo}
+              </p>
+              <p>
+                <h6>CreatedBy:</h6>
+                {bug.data.createdBy}
+              </p>
+              <Accordion>
+                <Card>
+                  <Accordion.Toggle
+                    as={Card.Header}
+                    variant="link"
+                    eventKey="0"
+                  >
+                    View attachements
+                  </Accordion.Toggle>
+                  <Accordion.Collapse eventKey="0">
+                    {bug.data.attachments.length > 0 ? (
+                      <Card.Body>Hello! I'm the body</Card.Body>
+                    ) : (
+                      <Card.Body>"No attachments found"</Card.Body>
+                    )}
+                  </Accordion.Collapse>
+                </Card>
+              </Accordion>
+            </Jumbotron>
+          </div>
+        )}
       </Container>
     </Fragment>
   );
